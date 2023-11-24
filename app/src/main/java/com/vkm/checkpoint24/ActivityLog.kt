782b868c5513481712a6a73f01dde97c6b557f65
Siamoza <siamoza@gmail.com>
2023-11-24 13:35:31 +0300

    0.0.5
    Добавлен второй экран

---
+++ app/src/main/java/com/vkm/checkpoint24/ActivityLog.kt
@@ -0,0 +1,11 @@
+package com.vkm.checkpoint24
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityLog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log)
+    }
+}
